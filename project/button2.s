020cce9628f16e50d0a813d24fb4e0b1b5fe7f6d
bacosta4 <bacosta4@miners.utep.edu>
2020-12-01 16:28:30 -0700

    Assembly working

--- project/button2.s
+++ project/button2.s
@@ -2,50 +2,45 @@
 	.p2align 1,0
 	.text
 
-	.extern toggle_on
-	.extern dim25
-	.extern dim50
-	.extern dim75
-
 	.data
 select:
 	.word 0
 ;;;  jt is constants (in flash)
 jt:
 	.word case0
-	.word default
 	.word case1
 	.word case2
 	.word case3
+	.word default
 
 	.global button2
 
 button2:
 	cmp #4, &select 	;range check 
-	jhs default		;jmp if select > 4
+	jhs default		;jmp if select >(-) 4
 
 	mov &select, R12
 	add R12, R12
 	mov jt(r12), r0
 	
 case0:
-	call &toggle_on		;toggle_on()
+	call #toggle_on		;toggle_on()
 	add #1, &select		;select++
 	jmp end			;break;
 	
 case1:
-	call &dim25		;dim25()
+	call #dim25		;dim25()
 	add #1, &select		;select++
 	jmp end			;break;
 	
 case2:
-	call &dim50		;dim50()
+	call #dim50		;dim50()
 	add #1, &select		;select++
 	jmp end			;break;
 	
 case3:
-	call &dim75		;dim75()
-	mov #0, &select		;select = 0;
+	call #dim75		;dim75()
+	add #1, &select		;select++
 	jmp end
 	
 default:
